32e19b4bfa562bd1cfb7978c34e884b7c2470e35
Vijay Bharvad <vijushepherd9@gmail.com>
2023-12-28 21:05:07 -0600

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,154 @@
+const logotext = "Vijay:)";
+const meta = {
+    title: "Vijay Bharvad",
+    description: "I’m Vijay Bharvad,Full stack devloper,currently working in Chicago",
+};
+
+const introdata = {
+    title: "I’m Vijay Bharvad",
+    animated: {
+        first: "Full-stack developer",
+        second: "I develop websites",
+    },
+    description: "",
+    your_img_url: "https://i.ibb.co/4VtFWMW/Viport.jpg",
+    resume_url: 'https://emerald-lauren-92.tiiny.site/',
+
+};
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "As a proactive and dedicated Computer Science student from the Illinois Institute of Technology, I possess a strong foundation in both theory and practical application of software development. I have honed a versatile skill set in languages and tools including HTML, CSS, Java, Python, React, Nodejs and PHP, complemented by hands-on experience with MySQL and MongoDB. My intern roles have enabled me to contribute significantly to projects, streamlining operations and enhancing user experiences. I excel in creating robust web applications and have a passion for leveraging technology to solve real-world problems.",
+};
+const worktimeline = [{
+        jobtitle: "Sogtware engineer intern",
+        where: "Infikey Technologies,",
+        date: "2021",
+    },
+    {
+        jobtitle: "Web Devloper intern",
+        where: "Finlark Technologies",
+        date: "2022",
+    },
+    {
+        jobtitle: "Full-Stack Develpper intern",
+        where: "Barodaweb",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "HTML",
+        value: 95,
+    },
+    {
+        name: "CSS",
+        value: 85,
+    },
+    {
+        name: "Java",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "Mongodb",
+        value: 85,
+    },
+    {
+        name: "MySql",
+        value: 85,
+    },
+    {
+        name: "Nodejs",
+        value: 85,
+    },
+    {
+        name: "Php",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Front-end developer",
+        description: "  Content coming soon.",
+    },
+    {
+        title: "Full-Stack Developer",
+        description: "Content coming soon.",
+    },
+   
+];
+
+const dataportfolio = [{
+        img: "https://i.ibb.co/wQF4hzv/smartt-resized.png?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        name: "Smart Homes",
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/gj3BNRw/ma2-resized.png/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        name: "Marwiz ",
+
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/drtVYsW/Homehub-resized.png/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        name: "Craftsman",
+
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/qWgK6Fg/sar2-resized.png?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        name: "Sarjani Industries",
+
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/zQ7zrYh/Sofa-and-chill-resized.png?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        name: "Sofa&chiil",
+
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/N9p2p2S/Digimappp-resized.png?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        name: "Digimap",
+
+        link: "#",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "vbharvad@hawk.iit.edu",
+    YOUR_FONE: "+1(571)604-2926",
+    description: "Hello there! Thanks for taking the time to explore my portfolio. If you have any questions, are interested in collaborating, or would like to discuss potential projects, I'd love to hear from you ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_iglx0nq",
+    YOUR_TEMPLATE_ID: "",
+    YOUR_USER_ID: "Vijushepherd9@gmail.com",
+};
+
+const socialprofils = {
+    github: "https://github.com/vijay0772",
+    linkedin: "https://www.linkedin.com/in/vijay-bharvad-a44085210/",
+};
+export {
+    contactConfig, dataabout,
+    dataportfolio, introdata, logotext, meta, services, skills, socialprofils, worktimeline
+};
